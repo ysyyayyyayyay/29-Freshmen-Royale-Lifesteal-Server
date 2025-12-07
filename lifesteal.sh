e6a836151560b5b4f22ef00add410c34e4b3f1b6
ysyyayyyayyay <landonzyin@gmail.com>
2025-12-07 19:41:45 +0000

    last

--- lifesteal.sh
+++ lifesteal.sh
@@ -1,2 +1,2 @@
 cd Lifesteal
-java -Xms8192M -Xmx8192M -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar server.jar 
+java -Xms6192M -Xmx6192M -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar server.jar 
